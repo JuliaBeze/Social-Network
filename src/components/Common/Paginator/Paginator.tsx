2927b7b3977e741ba05aecdd05ae1289c7ade67f
JuliaBeze <juliazhorova@rambler.ru>
2020-03-10 20:20:13 +0300

    paginator.tsx

--- src/components/Common/Paginator/Paginator.tsx
+++ src/components/Common/Paginator/Paginator.tsx
@@ -1,12 +1,20 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react'
 import styles from "./Paginator.module.css";
 
 
-let Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+type PropsType = {
+    totalUsersCount:number
+    pageSize:number
+    currentPage:number
+    onPageChanged:(pageNumber:number)=>void
+    portionSize:number
+}
+
+let Paginator:React.FC<PropsType> = ({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
 
     let pagesCount = Math.ceil(totalUsersCount / pageSize);
 
-    let pages = [];
+    let pages: Array<number> = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
